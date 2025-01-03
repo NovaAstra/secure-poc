82671ee0191994d7d30e68592c3f51cf2be78e7b
AugustGao <August.Gao@mspbots.ai>
2025-01-03 18:43:54 +0800

    main

--- core/src/main/java/com/nebula/core/controller/UserController.java
+++ core/src/main/java/com/nebula/core/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.nebula.core.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.nebula.common.common.BaseResponse;
