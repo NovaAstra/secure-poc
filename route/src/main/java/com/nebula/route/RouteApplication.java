12186d8d176f8d5223ec1efe7d0e1e6ad8ac2348
AugustGao <August.Gao@mspbots.ai>
2025-01-08 11:35:29 +0800

    main

--- route/src/main/java/com/nebula/route/RouteApplication.java
+++ route/src/main/java/com/nebula/route/RouteApplication.java
@@ -1,16 +1,16 @@
-package com.nebula.api;
+package com.nebula.route;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan("com.nebula.api.mapper")
+@MapperScan("com.nebula.route.mapper")
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiApplication {
+public class RouteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(RouteApplication.class, args);
 	}
 }
