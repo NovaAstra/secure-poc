47a1693e7906418a8a5eb4c866fa069b30a0ee5b
NovaAstra <NovaAstra@outlook.com>
2025-01-11 12:06:51 +0800

    main

--- fronted/src/main.js
+++ fronted/src/main.js
@@ -1,4 +1,22 @@
+import 'element-plus/dist/index.css'
+
 import { createApp } from 'vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/login', component: () => import("./Login.vue") },
+  { path: '/register', component: () => import("./Register.vue") },
+  { path: '/route', component: () => import("./Route.vue") },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
